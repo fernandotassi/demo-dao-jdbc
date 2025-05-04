6c04034efb6c43fd46ba19461036590e56b8c293
fernandotassi <apropriate3@gmail.com>
2025-05-04 00:10:10 -0300

    department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,57 @@
+package application;
+
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+import model.entities.Department;
+
+public class Program 
+{
+     public static void main(String[] args) 
+     {		 
+           Locale.setDefault(Locale.US);
+           Scanner scan = new Scanner(System.in);
+           Date d = new Date();
+           System.out.println(d);
+           
+           Department dep = new Department(1, "cama");
+           System.out.println(dep); 
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           scan.close();
+	 }
+}
